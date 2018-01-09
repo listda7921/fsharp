afa3f30ea7ec13e61921aa1d8ea94406de2f8320
dan list <listda7921@gmail.com>
2018-01-08 19:39:24 -0500

    init commit

---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,12 @@
+﻿open System.Windows.Forms
+
+type Person = { Name: string; Age: int}
+
+let testData = [|{ Name = "Harry"; Age = 37}; { Name = "Jully"; Age = 41}|]
+
+let form = new Form(Text = "F# Windows Form")
+
+let dataGrid = new DataGridView(Dock = DockStyle.Fill, DataSource = testData)
+form.Controls.Add(dataGrid)
+
+Application.Run(form)
